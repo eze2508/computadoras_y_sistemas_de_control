3b30a620fdc6513b1a67335ec6dc2fb78d887409
eze2508 <101151833+eze2508@users.noreply.github.com>
2024-05-09 09:44:28 -0300

    Update main.c

--- actividades/actividad_1/main.c
+++ actividades/actividad_1/main.c
@@ -1,4 +1,3 @@
-// Inclusion de librerias
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
@@ -6,9 +5,9 @@
 #include <math.h>
 
 // GPIO del ADC
-#define ANALOG_GPIO
+#define ANALOG_GPIO 27
 // Canal del ADC
-#define ANALOG_C
+#define ANALOG_C 0
 
 // Tiempo de refresco para el siete segmentos
 #define SLEEP_MS 10
@@ -16,11 +15,11 @@
 #define ADC_DELAY_MS  500
 
 // Constante de proporcionalidad para el NTC
-#define BETA
+#define BETA 3950.0
 // Resistencia a 25 grados del NTC
-#define RT0
+#define RT0 10000.0
 // Resistencia en serie al NTC
-#define RS
+#define RS 10000.0
 
 // Variable para almacenar el resultado del ADC
 uint16_t adc_value = 0;
@@ -33,9 +32,9 @@ float temperatura = 0.0;
  */
 bool muestreo_periodico(struct repeating_timer *t) {
   // Lectura analogica (variable adc_value)
-
+ uint16_t adc_value = adc_read();
   // Calcular valor de temperatura (variable temperatura)
-
+  temperatura = 1 / (log(1 / (4095. / adc_value - 1)) / BETA + 1.0 / 298.15) - 273.15;
 }
 
 /*
@@ -57,30 +56,40 @@ void display_temp(float temperatura) {
  * @brief Programa principal
  */
 int main() {
-  // Inicializacion de UART
-  stdio_init_all();
-  // Creo un repeating timer
-  struct repeating_timer timer;
-  // Creo un callback para la interrupcion del timer
-  add_repeating_timer_ms(ADC_DELAY_MS, muestreo_periodico, NULL, &timer);
-  // Inicializo ADC
+    // Inicializacion de UART
+    stdio_init_all();
 
-	// Inicializo GPIO26 como entrada analogica
+    // Creo un repeating timer
+    struct repeating_timer timer;
+    // Creo un callback para la interrupcion del timer
+    add_repeating_timer_ms(ADC_DELAY_MS, muestreo_periodico, NULL, &timer);
 
-  // Selecciono canal analogico
+    // Inicializo ADC
+    adc_init();
 
+    // Inicializo GPI027 como entrada analogica
+    adc_gpio_init(27);
 
-	// Solo si se usa LCD con I2C
-	// Configuro el I2C0 a 100 KHz de clock
+    // Selecciono canal analogico
+    adc_select_input(1);
 
-	// Elijo GPIO4 como linea de SDA
+    // Solo si se usa LCD con I2C
+    // Configuro el I2C0 a 100 KHz de clock
+    i2c_init(i2c0, 100 * 1000);
 
-	// Elijo GPIO5 como linea de SCL
+    // Elijo GPI04 como linea de SDA
+    gpio_set_function (4, GPIO_FUNC_I2C);
 
-	// Activo pull-up en ambos GPIO, son debiles por lo que
-	// es recomendable usar pull-ups externas
+    // Elijo GPI05 como linea de SCL
+    gpio_set_function(5, GPIO_FUNC_I2C);
 
-	// Inicializo display (si lo hubiese)
+    // Activo pull-up en ambos GPIO, son debiles por lo que
+    // es recomendable usar pull-ups externas
+    gpio_pull_up (4);
+    gpio_pull_up(5);
+
+    // Inicializo display (si lo hubiese)
+    lcd_init();
 
 
 	// Bucle infinito
